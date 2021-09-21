bc4fba9a13fcbc5decc7afe834f4d5a635ebb990
Leonardo <70207858+Leonardo-ST@users.noreply.github.com>
2021-09-21 17:32:36 +0000

    Colors adicionado, evento ready adicionado

--- Karina.js
+++ Karina.js
@@ -1,5 +1,13 @@
-const { Client, MessageEmbed } = require('discord.js')
+const { Client, MessageEmbed, Collection } = require('discord.js')
+const colors = require('colors')
 const karina = new Client({ intents:32767 })
+const config = require('../config.json')
+
+
+karina.on("ready", () => {
+console.log(colors.red(`[CLIENTE] Logado como ${karina.user.username}`))
+})
+
 
 
 karina.login(process.env.Karina)
